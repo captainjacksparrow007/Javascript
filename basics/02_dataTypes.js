f3251b39d53c5847a50c12a4cc89eb2962745940
Jayesh Kumar Malviya <83008629+captainjacksparrow007@users.noreply.github.com>
2024-03-25 18:47:43 +0000

    02

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" // treat all js code as new version
+
+// alert(3+3) //we use node.js not browser so that's why it shows the error
+
+let name="Jayesh"
+let age=21
+let isLoggedin= true
+let state;
+
+console.log(typeof undefined)
+console.log(typeof Null)
